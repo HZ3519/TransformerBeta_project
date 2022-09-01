d0486728c6d78bae35dd58a2273be2fa064282cc
Haowen Zhao <hz3519@ic.ac.uk>
2022-09-01 11:38:07 +0100

    pre_screen and reverse screen updated tested

--- Transformer_package_development/TransformerBeta/data_handling_tools.py
+++ Transformer_package_development/TransformerBeta/data_handling_tools.py
@@ -21,3 +21,61 @@ def window_divide(sequence, windown_size):
 		sequence_list.append(sequence_i)
 
 	return sequence_list
+
+
+
+def screen(validation_list, screen_list, window_size, return_target_condition = False, return_target_sequence = False, return_screen_condition = False, return_screen_sequence = False):
+
+	if return_target_condition == True or return_target_sequence == True:
+		target_condition_list = []
+		count = 0
+
+		for target in validation_list:
+			sub_target = window_divide(target, window_size)
+			target_condition = False
+
+			for sub in sub_target:
+				if sub in screen_list:
+					target_condition = True
+					break
+			target_condition_list.append(target_condition)
+
+			if count%10000 == 0:
+				print(count)
+			count += 1
+		target_condition_array = np.array(target_condition_list)
+		
+		if return_target_condition:
+			return target_condition_array
+		if return_target_sequence:
+			return validation_list[target_condition_array]
+		else:
+			raise ValueError("Please specify return_target_condition or return_target_sequence")
+
+	if return_screen_condition == True or return_screen_sequence == True:
+		screen_condition_list = []
+		count = 0
+
+		target_sequence_list = []
+		for target in validation_list:
+			sub_target = window_divide(target, window_size)
+			for sub in sub_target:
+				target_sequence_list += sub_target
+		
+		for screen in screen_list:
+			if screen in target_sequence_list:
+				screen_condition_list.append(True)
+			else:
+				screen_condition_list.append(False)
+		
+			if count%10000 == 0:
+				print(count)
+			count += 1
+		screen_condition_array = np.array(screen_condition_list)
+
+		if return_screen_condition:
+			return screen_condition_array
+		if return_screen_sequence:
+			return screen_list[screen_condition_array]
+		else:
+			raise ValueError("Please specify return_screen_condition or return_screen_sequence")
--- Transformer_package_development/TransformerBeta/model_train.py
+++ Transformer_package_development/TransformerBeta/model_train.py
@@ -216,14 +216,14 @@ def train_seq2seq(net, X_train, X_valid_len, Y_train, Y_valid_len, sample_weight
 
 
 
-def train_seq2seq_original(net, X_train, X_valid_len, Y_train, Y_valid_len, sample_weights, lr, num_epochs, batch_size, label_smoothing, amino_dict, device, warmup=1, model_name = 'model_demo', X_validation=None, X_validation_valid_len=None, Y_validation=None, Y_validation_valid_len=None):
+def train_seq2seq_training_steps(net, X_train, X_valid_len, Y_train, Y_valid_len, sample_weights, lr, training_steps, batch_size, label_smoothing, amino_dict, device, warmup=1, model_name = 'model_demo', X_validation=None, X_validation_valid_len=None, Y_validation=None, Y_validation_valid_len=None):
 	"""Train a model for sequence to sequence."""
 
 	def init_weights(module):
 		if isinstance(module, (nn.Linear)):
-			nn.init.normal_(module.weight, mean=0.0, std=0.01) 
+			nn.init.normal_(module.weight, mean=0.0, std=0.01)
 		if isinstance(module, (nn.Embedding)):
-			nn.init.normal_(module.weight, mean=0.0, std=0.01) 
+			nn.init.normal_(module.weight, mean=0.0, std=1e-5)
 
 	net.apply(init_weights)
 	net.to(device)
@@ -232,14 +232,17 @@ def train_seq2seq_original(net, X_train, X_valid_len, Y_train, Y_valid_len, samp
 	loss = MaskedSoftmaxCELoss()
 	net.train()
 
-	animator = d2l.Animator(xlabel='epoch', ylabel='loss')
+	animator = d2l.Animator(xlabel='training steps', ylabel='loss')
 	training_loss = []
 	validation_score_hamming = []
 	validation_score_sequence_accuracy = []
 
-
-	for epoch in range(num_epochs):
-		
+	# here we modify the training steps to be the number of epochs
+	current_step = 0
+	training_steps_count = []
+	
+	while current_step < training_steps:
+	
 		net.train()
 
 		timer = d2l.Timer()
@@ -266,19 +269,21 @@ def train_seq2seq_original(net, X_train, X_valid_len, Y_train, Y_valid_len, samp
 								device=device).reshape(-1, 1)
 			dec_input = torch.cat([bos, Y_train_minibatch[:, :-1]], 1)  
 			
-			Y_hat, _ = net(X_train_minibatch, dec_input, X_valid_len_minibatch)
+			Y_hat, _= net(X_train_minibatch, dec_input, X_valid_len_minibatch)
 			l = loss(Y_hat, Y_train_minibatch, Y_valid_len_minibatch, sample_weights_minibatch, label_smoothing)
-			l.backward()  
+			l.backward() 
 			d2l.grad_clipping(net, 1)
 			num_tokens = Y_valid_len_minibatch.sum()
 			optimizer.step()
 			scheduler.step()
 			with torch.no_grad():
 				metric.add(l*num_tokens, num_tokens) 
+			current_step += 1
 
-		animator.add(epoch + 1, (metric[0] / metric[1],))
+		animator.add(current_step, (metric[0] / metric[1],))
 		training_loss.append(metric[0] / metric[1])
-		print("epoch {}, loss: {}".format(epoch+1, metric[0] / metric[1]))
+		training_steps_count.append(current_step)
+		print("training step {}, loss: {}".format(current_step, metric[0] / metric[1]))
 
 		if X_validation is not None:
 			net.eval()
@@ -293,7 +298,7 @@ def train_seq2seq_original(net, X_train, X_valid_len, Y_train, Y_valid_len, samp
 
 				bos = torch.tensor([amino_dict['<bos>']] * Y_validation.shape[0],
 									device=device).reshape(-1, 1)
-				Y_true = torch.cat([bos, Y_validation[:, :-1]], 1)  
+				Y_true = torch.cat([bos, Y_validation[:, :-1]], 1)
 				Y_true = Y_true.type(torch.int32)
 
 				enc_outputs = net.encoder(X_validation, X_validation_valid_len)
@@ -302,10 +307,11 @@ def train_seq2seq_original(net, X_train, X_valid_len, Y_train, Y_valid_len, samp
 				dec_X = torch.tensor([amino_dict['<bos>']] * X_validation.shape[0],
 									device=device).reshape(-1, 1)
 
-				for i in range(Y_validation.shape[1]-1): 
+				for i in range(Y_validation.shape[1]-1):
 
 						Y_raw, dec_state = net.decoder(dec_X, dec_state)
 						Y = softmax_layer(Y_raw)
+
 						dec_X = Y.argmax(dim=2)
 						Y_pred = dec_X.type(torch.int32)
 
@@ -313,22 +319,21 @@ def train_seq2seq_original(net, X_train, X_valid_len, Y_train, Y_valid_len, samp
 				
 				hamming_scores = hamming_distance_training(Y_pred, Y_true)
 				validation_score_hamming.append(hamming_scores.cpu().numpy().item())
-				print("epoch {}, hamming distance: {}".format(epoch+1,hamming_scores))
+				print("training step {}, hamming distance: {}".format(current_step,hamming_scores))
 
 				sequence_accuracy_list = [torch.equal(a, b) for a, b in zip(Y_pred, Y_true)]
 				sequence_accuracy = sum(sequence_accuracy_list) / Y_true.shape[0]
 				validation_score_sequence_accuracy.append(sequence_accuracy)
-				print("epoch {}, sequence accuracy: {}".format(epoch+1,sequence_accuracy))
+				print("training step {}, sequence accuracy: {}".format(current_step,sequence_accuracy))
+				
 
 	print(f'loss {metric[0] / metric[1]:.3f}, {metric[1] / timer.stop():.1f} tokens/sec on {str(device)}')
 
-	# save training loss plot
 	plt.figure(figsize=(12, 8), facecolor=(1, 1, 1))
-	x = list(range(1, num_epochs+1))
+	x = np.array(training_steps_count)
 	file_time = time.strftime('%y%b%d_%I%M%p', time.gmtime())
-
 	plt.plot(x,training_loss,label="weighted_average_loss")
-	plt.xlabel('Epochs')
+	plt.xlabel('Training steps')
 	plt.ylabel('Weighted average loss')
 	plt.title("transformer <{}> training loss".format(model_name))
 	plt.grid()
@@ -336,33 +341,31 @@ def train_seq2seq_original(net, X_train, X_valid_len, Y_train, Y_valid_len, samp
 	plt.savefig('{}_lossplot_{}.png'.format(model_name, file_time))
 
 	if X_validation is not None:
-		# save validation hamming metic plot
+
 		plt.figure(figsize=(12, 8), facecolor=(1, 1, 1))
-		x = list(range(1, num_epochs+1))
+		x = np.array(training_steps_count)
 		plt.plot(x,validation_score_hamming,label="hamming_distance")
-		plt.xlabel('Epochs')
+		plt.xlabel('Training steps')
 		plt.ylabel('Hamming distance sum')
 		plt.title("transformer <{}> validation hamming scores".format(model_name))
 		plt.grid()
 		plt.legend()
 		plt.savefig('{}_validationplot_hamming_{}.png'.format(model_name, file_time))
-		
-		# save validation sequence accuracy plot
+
 		plt.figure(figsize=(12, 8), facecolor=(1, 1, 1))
-		x = list(range(1, num_epochs+1))
+		x = np.array(training_steps_count)
 		plt.plot(x,validation_score_sequence_accuracy,label="sequence_accuracy")
-		plt.xlabel('Epochs')
+		plt.xlabel('Training steps')
 		plt.ylabel('Sequence accuracy')
 		plt.title("transformer <{}> validation sequence accuracy".format(model_name))
 		plt.grid()
 		plt.legend()
 		plt.savefig('{}_validationplot_sequence_accuracy_{}.png'.format(model_name, file_time))
+	
 
-	# save model weights
 	file_name = model_name + '_' + file_time
 	torch.save(net.state_dict(), file_name)
-
-	# save training_loss, validation_score_hamming, validation_score_sequence_accuracy
+	
 	np.savetxt('{}_training_loss_{}.csv'.format(model_name, file_time), training_loss, delimiter=',', fmt='%s')
 	if X_validation is not None:
 		np.savetxt('{}_validation_score_hamming_{}.csv'.format(model_name, file_time), validation_score_hamming, delimiter=',', fmt='%s')
